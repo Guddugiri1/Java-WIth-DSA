15f87e0e682e0bb637decf30806e41b1d5a8e2e8
Guddu Giri <guddugiri216@.com>
2024-08-05 03:02:17 +0530

    add

---
+++ Day 1/DataTypes.java
@@ -0,0 +1,14 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        byte b = 10;
+        System.out.println(b);
+        char ch ='a';
+        System.out.println(ch);
+       // boolean var = false;
+       // float price = 10.11;
+       // int nmber = 25;
+        //long
+        //doule
+       // short n = 240;
+    }
+}
---
+++ Day 1/PrintVar.java
@@ -0,0 +1,13 @@
+public class PrintVar {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        System.out.println(a);
+        System.out.println(b);
+        String name = "Guddu Giri";
+        System.out.println(name);
+
+        a= 60;
+        System.out.println(a);
+    }
+}
