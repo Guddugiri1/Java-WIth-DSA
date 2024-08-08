3c7531c25c6730aa9672f7badc83c066f42ebe2f
Guddu Giri <guddugiri216@.com>
2024-08-09 02:36:28 +0530

    add new code

---
+++ Day 1/TypeConversion1.java
@@ -0,0 +1,11 @@
+
+import java.util.Scanner;
+
+public class TypeConversion1 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float number = sc.nextFloat(); // Use nextFloat to read a float directly
+        System.out.println(number);
+    }
+}
