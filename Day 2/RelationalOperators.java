22a723f26b7ccc2c1fbaa357e07710545b8bfc1f
Guddu Giri <guddugiri216@.com>
2024-08-11 00:45:51 +0530

    Day 2 Code

---
+++ Day 2/RelationalOperators.java
@@ -0,0 +1,25 @@
+
+public class RelationalOperators {
+
+    public static void main(String[] args) {
+        // int A = 10;
+        // int B = 5;
+        // System.out.println(A == B);
+
+        // int A = 10;
+        // int B = 10;
+        // System.out.println(A == B);
+        // int A = 10;
+        // int B = 10;
+        // System.out.println(A != B);
+        // int A = 10;
+        // int B = 5;
+        // System.out.println(A != B);
+        // int A = 10;
+        // int B = 5;
+        // System.out.println(A > B);
+        int A = 10;
+        int B = 5;
+        System.out.println(A >= B);
+    }
+}
